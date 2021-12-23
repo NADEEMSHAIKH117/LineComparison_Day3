ae486279ff071e80a6b353fda0522de7dabe67a6
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-12-23 09:23:18 +0530

    UC2-EqualLength

--- LineComparison.php
+++ LineComparison.php
@@ -1,6 +1,7 @@
 <?php
 
-class LineComparison {
+class LineComparison
+{
 
     //creating public variable
     public $pointX1;
@@ -9,27 +10,48 @@ class LineComparison {
     public $pointY2;
 
     //creating function to get user input
-    public function getUserInput() {
+    public function getUserInput()
+    {
         $this->pointX1 = readline("Enter line X1 coordinates : ");
         $this->pointX2 = readline("Enter line X2 coordinates : ");
         $this->pointY1 = readline("Enter line Y1 coordinates : ");
         $this->pointY2 = readline("Enter line Y2 coordinates : ");
     }
 
-    //creating function to store the values of points in distance and returning the distance
-    public function lengthOfLine() {
+      /**
+	 * lengthOfLine method is used to calculate length of line
+	 * @return distance
+	 */
+    public function lengthOfLine()
+    {
         //using pow() and sqrt() in build function
         // formula for distance d = \sqrt{(x_2 - x_1)^2 + (y_2-y_1)^2}
         $distance = sqrt(
             pow(($this->pointX2 - $this->pointX1), 2) +
-            pow(($this->pointY2 - $this->pointY1), 2)
-        );
+            pow(($this->pointY2 - $this->pointY1), 2));
         return $distance;
+    }
+
+    //creating checkEquality function to compare both line and check lines are equal or not
+    public function checkEquality($line1, $line2) {
+        if ($line1 == $line2) {
+            echo "both line length are equal";
+        } else {
+            echo "lines length are different";
+        }
     }
 }
 
 //creating object of LineComparison class
 $calculateLength = new LineComparison();
 $calculateLength->getUserInput();
-echo "Length of line is : " .$calculateLength->lengthOfLine();
+$line1 = $calculateLength->lengthOfLine();
+echo "Length of line1 is : " . round($line1, 2) . "\n";
+
+$calculateLength->getUserInput();
+$line2 = $calculateLength->lengthOfLine();
+echo "Length of line2 is :"  . round($line2, 2) . "\n";
+
+$calculateLength->checkEquality($line1, $line2);
+
 ?>
